7d5c80c216a830f9c5e054fde95c8149283e8bdf
Kit Lee <7000003+wingkitlee0@users.noreply.github.com>
2025-03-29 16:28:27 -0400

    chore: renaming to pyigrf2 to avoid collision

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,14 @@
-import pyigrf
+import pyigrf2
+
 
 def test_0001():
-    print(pyigrf._igrf.__dict__)
-    print(pyigrf.igrf14syn.__doc__)
+    print(pyigrf2._igrf.__dict__)
+    print(pyigrf2.igrf14syn.__doc__)
 
-    assert False
+    assert True
+
+
+def test_0002():
+    x, y, z, f = pyigrf2.igrf14syn(0, 2024.0, 1, 1, 0, 0)
+
+    assert x * y * z * f != 0
